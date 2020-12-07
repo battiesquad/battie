594c9b0ad02cb958220dba7c99b66170aaa4b730
rutger1501 <71037167+rutger1501@users.noreply.github.com>
2020-12-07 10:20:27 +0100

    build repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# battiebot
+een simpele discord bot (leren) maken
+
+Hoe run je de bot:
+
+1. Installeer Node.JS
+2. Installeer Node Package Manager (NPM)
+3. Open de battiebot folder in vscode
+4. Open een terminal in vscode
+5. voer 'npm install' uit in de terminal en wacht tot alle packages geïnstalleerd zijn
+6. voer 'npm start' uit
---
+++ main.ts
@@ -0,0 +1,57 @@
+import Discord, { TextChannel } from "discord.js";
+import { Command } from "./models/Command";
+import { rolesCommands } from "./modules/roles/roles-module";
+import { soundsCommands } from "./modules/sounds/sounds-module";
+import { timerCommands } from "./modules/timers/timer-module";
+
+// Setup discord client
+const client = new Discord.Client();
+const COMMAND_PREFIX = "-";
+
+// Get all desired commands that the server should handle
+const commandList: Command[] = [
+    ...rolesCommands,
+    ...timerCommands,
+    ...soundsCommands,
+];
+
+// Make a collection of all the commands the server should handle
+const commands = new Discord.Collection<string, Command>();
+
+// Fill the commands collection
+commandList.forEach((command) => commands.set(command.name, command));
+
+client.once("ready", () => {
+    console.log("Battiebot is aanwezig");
+
+    client.channels.fetch("658627143344848907").then((channel) => {
+        // Regard the channel as a text channel
+        const TextChannel = channel as TextChannel;
+
+        // Send introduction message
+        // TextChannel.send(`De battiebot is online <:pog:735838091788550164>`);
+    });
+});
+
+client.on("message", (message) => {
+    // Check whether bot should ignore incoming message
+    if (!message.content.startsWith(COMMAND_PREFIX) || message.author.bot)
+        return;
+
+    // Get the command and arguments
+    const args = message.content.slice(COMMAND_PREFIX.length).split(/ +/);
+
+    // Get the command and remove it from args array
+    const command = args.shift()?.toLowerCase();
+    if (!command) return;
+
+    // Get the desired client command
+    const clientCommand = commands.get(command);
+
+    // If command exists, execute it
+    if (clientCommand) clientCommand.execute(message, args);
+    // Otherwise return
+    else return;
+});
+
+client.login("NzgyMDI1OTI4OTc2MzY3NjY3.X8GMZg.dJX4ixs_oVqVck9dPr3X7wWGDCc");
